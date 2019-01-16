ce1af061593689015811cbba92e4af50c4c1efdd
GaganLal <lalgagan@outlook.com>
2019-01-16 02:28:56 +0000

    first push before last step

---
+++ express_server.js
@@ -0,0 +1,74 @@
+var express = require("express")
+
+var app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var PORT = 8081;
+
+app.set("view engine", "ejs")
+
+var urlDataBase = {
+
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+
+};
+
+
+app.get("/", (req, res) => {                                            //YOU ARE ADDING ADDITIONAL ENDPOINTS through adding paths
+  res.send("Hello!")
+
+})
+
+app.get("/urls.json", (req,res) => res.json(urlDataBase))               //get retrieves something, using the PATH.
+                                                                        //respond with json version of urlDataBase if u use /urls.json path
+
+app.get("/hello", (req,res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")              //.send is basically saying post this on the page when u search it
+})
+
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDataBase
+   };                         //passing the urls data (urlDataBase) to the template urls_index
+  res.render("urls_index", templateVars);                                 //by storing as variable templateVars
+});
+
+app.get("/urls/new", (req,res) => {
+  res.render("urls_new")
+
+})
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    shortURL : req.params.id,
+    longURL : urlDataBase
+  }
+  res.render('urls_show', templateVars)
+})
+
+app.post("/urls", (req, res) => {
+  console.log(req.body.longURL);                            // KEY PART IS THAT CONSOLE.LOG ON TERMINALWHAT U SUBMIT. BUT IF U DONT
+  res.send("Ok");                                // {longURL : WHAT U TYPED IN form }  // .longurl u get the value of it which is what
+});                                                                   // u submit
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on Port ${PORT}`)
+})
+
+
+function generateRandomString() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 6; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+console.log(generateRandomString())
+
